883c36fc95478c98494f4bdd7219f64e7aa16126
AndersonShen <shen.andersonshen@gmail.com>
2022-11-14 02:43:00 +0800

    動態調整 c3 內容

--- all.js
+++ all.js
@@ -10,41 +10,19 @@
 //     price: 1400,
 //     rate: 10,
 //   },
-//   {
-//     id: 1,
-//     name: "貓空纜車雙程票",
-//     imgUrl:
-//       "https://images.unsplash.com/photo-1501393152198-34b240415948?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1650&q=80",
-//     area: "台北",
-//     description: "乘坐以透明強化玻璃為地板的「貓纜之眼」水晶車廂，享受騰雲駕霧遨遊天際之感",
-//     group: 99,
-//     price: 240,
-//     rate: 2,
-//   },
-//   {
-//     id: 2,
-//     name: "台中谷關溫泉會1日",
-//     imgUrl:
-//       "https://images.unsplash.com/photo-1535530992830-e25d07cfa780?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1650&q=80",
-//     area: "台中",
-//     description:
-//       "全館客房均提供谷關無色無味之優質碳酸原湯，並取用八仙山之山冷泉供蒞臨貴賓沐浴及飲水使用。",
-//     group: 20,
-//     price: 1765,
-//     rate: 7,
-//   },
 // ];
 
 const apiUrl = `https://raw.githubusercontent.com/hexschool/js-training/main/travelApi.json`;
 let productData = [];
 
 const areaData = ["台北", "台中", "高雄"];
+let c3Data = [];
 
 const trainingForm = document.querySelector(".training-form"); // 表單
 const ticketCardArea = document.querySelector(".ticketCard-area"); // 套票卡片區
 const searchTicketRegion = document.querySelector("#searchTicketRegion"); // 套票卡片區
 const searchLength = document.querySelector("#searchLength"); // 查詢結果數量
-const cantFindArea = document.querySelector(".cantFind-area"); // 查詢結果為 0 
+const cantFindArea = document.querySelector(".cantFind-area"); // 查詢結果為 0
 
 trainingForm.addEventListener("submit", (e) => {
   e.preventDefault();
@@ -56,12 +34,11 @@ searchTicketRegion.addEventListener("change", (e) => {
 });
 
 // 讀取 API
-async function getData() { 
-  try { 
+async function getData() {
+  try {
     let { data } = await axios.get(apiUrl);
     productData = data.data;
-  }
-  catch (err) { 
+  } catch (err) {
     console.error(err);
   }
 }
@@ -79,22 +56,47 @@ function submitForm() {
     rate: trainingForm[5].value,
   };
   productData.push(newData);
+  calcAreaNumber();
+  c3Init();
   renderTicketCardArea([...productData]);
   trainingForm.reset();
 }
 
+// 統計各套票的地區數量，並整理成 c3 的格式
+function calcAreaNumber() {
+  // 1. 統計各套票的地區數量
+  // 2. 整理成 c3 的格式 [ ["高雄", 2], ["台北",1], ["台中", 1] ]
+  c3Data = [];
+  let tempObj = {};
+  productData.forEach((item) => {
+    if (tempObj[item.area] === undefined) {
+      tempObj[item.area] = 1;
+    } else {
+      tempObj[item.area] += 1;
+    }
+  });
+  const areaLabel = Object.keys(tempObj);
+  areaLabel.forEach((item) => {
+    let temp = [];
+    temp.push(item);
+    temp.push(tempObj[item]);
+    c3Data.push(temp);
+  });
+  console.log(c3Data);
+}
+
 // 分類
-function filterTicketCardData(selectLocation = '') {
+function filterTicketCardData(selectLocation = "") {
   let ticketList = productData;
   if (selectLocation) {
-    ticketList = productData.filter((item) => item.area === selectLocation)
-  } 
+    ticketList = productData.filter((item) => item.area === selectLocation);
+  }
   if (ticketList.length) {
-    ticketCardArea.classList.remove('d-none');
-    cantFindArea.classList.add('d-none');
-  } else { 
-    ticketCardArea.classList.add('d-none');
-    cantFindArea.classList.remove('d-none');
+    ticketCardArea.classList.remove("d-none");
+    cantFindArea.classList.add("d-none");
+  } else {
+    ticketCardArea.classList.add("d-none");
+    cantFindArea.classList.remove("d-none");
   }
   renderTicketCardArea(ticketList);
 }
@@ -132,42 +134,39 @@ function renderTicketCardArea(ticketList) {
         `;
   });
   ticketCardArea.innerHTML = totalTicketCardStr;
-  searchLength.innerHTML = `本次搜尋共 ${ticketList.length} 筆資料`
+  searchLength.innerHTML = `本次搜尋共 ${ticketList.length} 筆資料`;
 }
 
 // C3
-function c3Init() { 
+function c3Init() {
   c3.generate({
-    bindto: '#chart', // HTML 元素綁定
+    bindto: "#chart", // HTML 元素綁定
     data: {
-      columns: [
-        ['高雄', 2],
-        ['台中', 1],
-        ['台北',1]
-      ], // 資料存放
-      type:"donut", // 圖表種類
-      colors:{
-        "高雄":"#E68618",
-        "台中":"#5151D3",
-        "台北":"#26BFC7"
-      }
+      columns: c3Data, // 資料存放
+      type: "donut", // 圖表種類
+      colors: {
+        高雄: "#E68618",
+        台中: "#5151D3",
+        台北: "#26BFC7",
+      },
     },
     size: {
       width: 160,
-      height: 160
+      height: 160,
     },
     donut: {
       title: "套票地區比重",
       width: 8,
       label: {
-        show: false
-      }
-    }
-});
+        show: false,
+      },
+    },
+  });
 }
 
 async function init() {
   await getData();
+  calcAreaNumber();
   filterTicketCardData();
   c3Init();
 }
